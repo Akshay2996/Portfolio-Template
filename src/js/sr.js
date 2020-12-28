52de629f0b890400f39dc7a7a25ecf90dd9c700e
Akshay Sharma <akshaysharmaay@gmail.com>
2020-12-29 01:47:16 +0530

    changed the animation delay

--- src/js/sr.js
+++ src/js/sr.js
@@ -21,7 +21,7 @@ export default function() {
   ScrollReveal().reveal('.hero-title', 
     assignProps(
       { 
-        delay: 500, 
+        delay: 800, 
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -29,7 +29,7 @@ export default function() {
   ScrollReveal().reveal('.hero-cta', 
     assignProps(
       {
-        delay: 700, 
+        delay: 800, 
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -38,7 +38,7 @@ export default function() {
   ScrollReveal().reveal('.about-wrapper__image', 
     assignProps(
       {
-        delay: 600,
+        delay: 800,
         origin: 'bottom'
       }, defaultProps)
   );
@@ -46,7 +46,7 @@ export default function() {
   ScrollReveal().reveal('.about-wrapper__info', 
     assignProps(
       {
-        delay: 700,
+        delay: 800,
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -55,7 +55,7 @@ export default function() {
   ScrollReveal().reveal('.project-wrapper__text', 
     assignProps(
       {
-        delay: 500,
+        delay: 800,
         origin: window.innerWidth > 768 ? 'left' : 'bottom'
       }, defaultProps)
   );
@@ -63,7 +63,7 @@ export default function() {
   ScrollReveal().reveal('.project-wrapper__image', 
     assignProps(
       {
-        delay: 700,
+        delay: 800,
         origin: window.innerWidth > 768 ? 'right' : 'bottom'
       }, defaultProps)
   );
@@ -72,7 +72,7 @@ export default function() {
   ScrollReveal().reveal('.contact-wrapper', 
     assignProps(
       {
-        delay: 500,
+        delay: 800,
         origin: 'bottom'
       }, defaultProps)
   );
